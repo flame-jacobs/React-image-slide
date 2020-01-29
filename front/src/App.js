ed9418c0adfc5e1aefc74823419948dd413a780f
Kyle Jacobs <50624144+flame-jacobs@users.noreply.github.com>
2020-01-29 19:52:04 +0200

    Add files via upload

---
+++ front/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Slide from './components/slide';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <h1>Legendary Pokemon Slider </h1> */}
+        <Slide/>
+
+    </div>
+  );
+}
+
+export default App;
